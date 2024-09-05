02fa902176b5254f17142516940de2aad876a951
Denys Koval <denissskoval@gmail.com>
2024-09-05 15:07:45 +0200

    first commit

---
+++ src/main/java/de/ait/platform/PlatformApplication.java
@@ -0,0 +1,13 @@
+package de.ait.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/ait/platform/PlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ait.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

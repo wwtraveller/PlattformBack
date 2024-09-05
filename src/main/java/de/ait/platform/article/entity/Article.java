00f1c058aa9f971637e2100bf8534db846148c62
alisaTonga <tongalise@gmail.com>
2024-09-05 16:15:13 +0200

    add entity without relation

---
+++ src/main/java/de/ait/platform/article/entity/Article.java
@@ -0,0 +1,32 @@
+package de.ait.platform.article.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+@Entity
+@Table(name="articles")
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "photo")
+    private String photo;
+
+
+}

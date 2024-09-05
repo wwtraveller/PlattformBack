08fdc1bcf4f94029d95578f5091f927ec7f62c6a
alisaTonga <tongalise@gmail.com>
2024-09-05 15:59:06 +0200

    add category

--- src/main/java/de/ait/platform/category/entity/Category.java
+++ src/main/java/de/ait/platform/category/entity/Category.java
@@ -1,4 +1,25 @@
 package de.ait.platform.category.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+@Entity
+@Table(name="categories")
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
 }

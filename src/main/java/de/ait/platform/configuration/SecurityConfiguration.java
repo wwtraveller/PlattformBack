09d6f0496dc1c59e3598225f911db3fba5712a66
Denys Koval <denissskoval@gmail.com>
2024-09-12 16:39:24 +0200

    update securityFilterChain

--- src/main/java/de/ait/platform/configuration/SecurityConfiguration.java
+++ src/main/java/de/ait/platform/configuration/SecurityConfiguration.java
@@ -28,9 +28,25 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests(
                         x -> x
                                 .requestMatchers(HttpMethod.GET, "/api/articles").permitAll()
-                                .requestMatchers(HttpMethod.GET, "/api/articles/{id}").hasAnyRole("USER", "ADMIN")
+                                .requestMatchers(HttpMethod.GET, "/api/articles/{id}").permitAll()
                                 .requestMatchers(HttpMethod.POST, "/api/articles").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.PUT, "/api/articles").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.DELETE, "/api/articles/{id}").hasRole("ADMIN")
                                 .requestMatchers(HttpMethod.GET, "/api/categories").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/categories/{id}").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/api/categories").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.PUT, "/api/categories").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.DELETE, "/api/categories/{id}").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.GET, "/api/users").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/users/{id}").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/api/users").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.PUT, "/api/users").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.DELETE, "/api/users/{id}").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.GET, "/api/comments").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/comments/{id}").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/api/comments").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.PUT, "/api/comments").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.DELETE, "/api/comments/{id}").hasRole("ADMIN")
                                 .anyRequest().authenticated()
                 ).httpBasic(Customizer.withDefaults());
 

73b1d8af196c505e282dde087873b3b02476d1e8
alisaTonga <tongalise@gmail.com>
2024-09-25 11:09:22 +0200

    add status to error article

--- src/main/java/de/ait/platform/article/service/ArticleServiceImp.java
+++ src/main/java/de/ait/platform/article/service/ArticleServiceImp.java
@@ -131,7 +131,15 @@ public class ArticleServiceImp implements ArticleService {
 //        if (fingByTitle(dto.getTitle()).if) {
 //            throw new FieldIsTaken("That title already exist");
 //        }
+        boolean isEmpty = repository.findAll()
+                .stream()
+                .filter((dto.getTitle().equals("")) ? a -> true : article -> article.getTitle().equalsIgnoreCase(dto.getTitle()))
+                .toList()
+                .isEmpty();
 
+        if (!isEmpty){
+            throw new FieldIsTaken("Title: " + dto.getTitle() + " is already taken");
+        }
         Article existingArticle = repository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Article not found with id: " + id));
 

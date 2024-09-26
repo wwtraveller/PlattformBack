ec126d5573ba65df8bab2a53dc1cd1f72207db27
Taras <taraschaikovskii556@gmail.com>
2024-09-26 09:16:42 +0200

    swagerFinal

--- src/main/java/de/ait/platform/article/dto/ResponseArticle.java
+++ src/main/java/de/ait/platform/article/dto/ResponseArticle.java
@@ -3,6 +3,7 @@ package de.ait.platform.article.dto;
 import de.ait.platform.category.entity.Category;
 import de.ait.platform.comments.entity.Comment;
 import de.ait.platform.user.entity.User;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,13 +15,28 @@ import java.util.Set;
 @NoArgsConstructor
 @Getter
 @Setter
+@Schema(description = "DTO for the response containing article details")
 public class ResponseArticle {
+
+    @Schema(description = "Unique identifier of the article", example = "1", required = true)
     private Long id;
+
+    @Schema(description = "Title of the article", example = "Getting Started with Spring Boot", required = true)
     private String title;
+
+    @Schema(description = "Content of the article", example = "This article explains the basics of Spring Boot...")
     private String content;
+
+    @Schema(description = "URL to the article's photo", example = "https://example.com/photo.jpg")
     private String photo;
+
+    @Schema(description = "Username of the article's author", example = "john_doe")
     private String username;
+
+    @Schema(description = "Set of comments associated with the article")
     private Set<Comment> comments;
+
+    @Schema(description = "Set of categories associated with the article")
     private Set<Category> categories;
 
 }

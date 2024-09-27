80b0b5628e12cb57839011101eb4403d4286b2eb
Taras <taraschaikovskii556@gmail.com>
2024-09-27 11:45:44 +0200

    TestCategoryRepo

---
+++ src/test/java/de/ait/platform/user/reposittory/UserRepositoryTest.java
@@ -0,0 +1,28 @@
+package de.ait.platform.user.reposittory;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserRepositoryTest {
+
+    @Test
+    void existsByEmail() {
+    }
+
+    @Test
+    void existsByUsername() {
+    }
+
+    @Test
+    void findUserByUsername() {
+    }
+
+    @Test
+    void findUserById() {
+    }
+
+    @Test
+    void findByEmail() {
+    }
+}
